be0e142eee896c121202f30fde5dea17d5b1e41e
Laks96 <lazarspasic96@gmail.com>
2020-10-12 01:59:52 +0200

    Updates

---
+++ precache-manifest.e22c31f18bd3d3787ae94359b43e8363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301c690dee0d0807a1f4bd89cb83ec2c",
+    "url": "/BIT-PEOPLE/index.html"
+  },
+  {
+    "revision": "f39464443d6263f1a34d",
+    "url": "/BIT-PEOPLE/static/css/main.29e32608.chunk.css"
+  },
+  {
+    "revision": "3ac9b8a1bebb9b59084e",
+    "url": "/BIT-PEOPLE/static/js/2.3332ba9c.chunk.js"
+  },
+  {
+    "revision": "f39464443d6263f1a34d",
+    "url": "/BIT-PEOPLE/static/js/main.26acbabe.chunk.js"
+  },
+  {
+    "revision": "7f5123a93e2aef2fd356",
+    "url": "/BIT-PEOPLE/static/js/runtime-main.ffed5537.js"
+  }
+]);
